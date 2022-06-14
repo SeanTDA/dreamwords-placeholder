d9d13f5759c0b0dc5358d654745d4693c306c566
Sean Simon <ssimon@altvfx.local>
2022-06-14 15:08:29 +1000

    + updated rounding to floor

--- python/image_manager.py
+++ python/image_manager.py
@@ -1,19 +1,19 @@
 
 import os, shutil
 import json
-
+import math
 
 def getMetadataCodeFromDay(dayIndex):
   i = dayIndex + 15
   doopsie = chr((i%15)+97)
   lottie = (i*6) % 12
   dollop = chr(((i*2) % 10)+105)
-  sam = 500 - (round((i+569.3)*46.85) % 105)
-  danthony = round((67.5+i) * 978.54) % 101
-  greg = round(7+((i*12.3)%5.5)+12327)%56781
+  sam = 500 - (math.floor((i+569.3)*46.85) % 105)
+  danthony = math.floor((67.5+i) * 978.54) % 101
+  greg = math.floor(7+((i*12.3)%5.5)+12327)%56781
   dro = chr((i%9)+107)+chr((i%12)+108)+chr(103+(i%17))
-  pebble = round(i*6.14159) % 9684
-  plub = round(i*11.93) % 2680
+  pebble = math.floor(i*6.14159) % 9684
+  plub = math.floor(i*11.93) % 2680
   stromboli =  str(lottie) + str(greg) + dro + str(pebble) + str(plub) + dollop + str(sam) + str(danthony)
   if len(stromboli) % 2 == 1:
     stromboli = doopsie + stromboli
@@ -25,12 +25,12 @@ def getImageCodeFromDay(dayIndex):
   doopsie = chr((i%26)+97)
   lottie = (i*5) % 16
   dollop = chr(((i*2) % 19)+105)
-  sam = 500 - (round((i+19.3)*46.85) % 305)
-  danthony = round((67.5+i) * 978.54) % 101
+  sam = 500 - (math.floor((i+19.3)*46.85) % 305)
+  danthony = math.floor((67.5+i) * 978.54) % 101
   greg = (7+i+12327)%56781
   dro = chr((i%9)+107)+chr((i%12)+108)+chr(103+(i%17))
-  pebble = round(i*3.14159) % 9684
-  plub = round(i*18.93) % 1680
+  pebble = math.floor(i*3.14159) % 9684
+  plub = math.floor(i*18.93) % 1680
   stromboli = str(lottie) + dollop + str(sam) + str(danthony) + str(greg) + dro + str(pebble) + str(plub)
   if len(stromboli) % 2 == 0:
     stromboli = doopsie + stromboli
@@ -128,6 +128,7 @@ if initialDay == "":
 processFiles(preprocessedDir, preprocessedConvertedDir, initialDay)
 
   
+#print("metadata_"+getMetadataCodeFromDay(15))
   
 '''
 print("")
